afe2d0472c05ce9b16348c9fe0521eee286926b6
simo hamzaoui <simo.hamzaoui@gmail.com>
2026-02-11 09:34:30 +0100

    On Premise version

--- custom/Extension/modules/Administration/Ext/Administration/archive_email.php
+++ custom/Extension/modules/Administration/Ext/Administration/archive_email.php
@@ -5,7 +5,7 @@ $admin_option_defs['Administration']['ArchiveEmailPanel'] = [
   'Administration',
   'LBL_ARCHIVE_EMAIL_TITLE',
   'LBL_ARCHIVE_EMAIL_DESC',
-  '/#gen_configs'
+  'javascript:void(parent.SUGAR.App.router.navigate("gen_configs", {trigger: true}));'
 ];
 $admin_group_header[] = [
   'LBL_ARCHIVE_EMAIL_HEADER',
